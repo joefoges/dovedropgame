4d84b2223618823e17e90433f1c6051a1c51d961
joefoges <steams_knock.7t@icloud.com>
2025-06-17 20:52:23 -0400

    Update Dove.js

--- Dove/Dove.js
+++ Dove/Dove.js
@@ -7,16 +7,23 @@ export default class Dove extends Sprite {
     super(...args);
 
     this.costumes = [
-      new Costume("dove-a", "./Dove/costumes/dove-a.svg", { x: 59.555845000000005, y: 52.34710172799984 }),
+      new Costume("dove-a", "./Dove/costumes/dove-a.svg", {
+        x: 59.555845000000005,
+        y: 52.34710172799984
+      }),
       new Costume("dove-b", "./Dove/costumes/dove-b.svg", { x: 88, y: 57 })
     ];
 
     this.sounds = [new Sound("bird", "./Dove/sounds/bird.wav")];
 
+    // Shrink the dove to 50 % of its original size so it appears smaller on stage.
+    // (Scratch/Leopard sprites use a 100‑based percentage for size.)
+    this.size = 50;
+
     this.triggers = [
       new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked),
       new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked2),
-      new Trigger(Trigger.GREEN_FLAG, this.followMouse) // New trigger to enable mouse‑controlled movement
+      new Trigger(Trigger.GREEN_FLAG, this.followMouse) // Keeps dove glued to the mouse pointer
     ];
   }
 
@@ -50,11 +57,10 @@ export default class Dove extends Sprite {
     }
   }
 
-  // New coroutine: keep the dove glued to the mouse position every tick
   *followMouse() {
     while (true) {
       this.goto(this.mouse.x, this.mouse.y);
-      yield; // Let the engine breathe before the next frame
+      yield; // let the engine breathe before the next frame
     }
   }
 }
